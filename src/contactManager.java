e258be1d069ee5ac1dda6cba15b9f5a19f4b6c1e
jordy muniz <jordy.muniz96@gmail.com>
2021-01-09 15:08:34 -0600

    Got the main menu to display

--- src/contactManager.java
+++ src/contactManager.java
@@ -1,3 +1,19 @@
+import java.awt.*;
+import java.util.Scanner;
+
+
 public class contactManager {
 
+    public static int Menu(){
+        Scanner input =  new Scanner(System.in);
+        System.out.println(1 + "." + " View contacts.");
+        System.out.println(2 + "." + " Add a new contact.");
+        System.out.println(3 + "." + " Search a contact by name.");
+        System.out.println(4 + "." + " Delete an existing contact.");
+        System.out.println(5 + " Exit.");
+        System.out.println("Enter an option (1, 2, 3, 4, or 5)");
+        int Choice = input.nextInt();
+
+        return Choice;
+    }
 }
