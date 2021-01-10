0c4dabc8f86b1e4b5cb7d659046c7ee65754e540
jordy muniz <jordy.muniz96@gmail.com>
2021-01-09 23:43:20 -0600

    can now display contacts

--- src/contactManager.java
+++ src/contactManager.java
@@ -1,17 +1,12 @@
-import java.awt.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.ArrayList;
-
-
-
+import java.util.List;
 public class contactManager {
 
-    public static int Menu(){
+    public static int Menu() throws IOException {
         Scanner input =  new Scanner(System.in);
         System.out.println(1 + "." + " View contacts.");
         System.out.println(2 + "." + " Add a new contact.");
@@ -20,20 +15,29 @@ public class contactManager {
         System.out.println(5 + " Exit.");
         System.out.println("Enter an option (1, 2, 3, 4, or 5)");
         int Choice = input.nextInt();
-        if(Choice == 5){
+
+        if(Choice == 1){
+            viewContacts();
+            System.out.println("Would you like to do something else? If so enter another option!");
+        }else if(Choice == 5){
             System.out.println("Goodbye");
+            System.exit(0);
         }
-        return Choice;
+        return Menu();
     }
 
-//    public static void viewContacts(){
-//        Path contactsPath = Paths.get("data", "contacts.txt");
-//        List<String> contactsList = Files.readAllLines(contactsPath);
-//
-//        for (int i = 0; i < contactsList.size(); i += 1) {
-//            System.out.println((i + 1) + ": " + contactsList.get(i));
-//        }
+    public static void viewContacts() throws IOException {
+        Path contactsPath = Paths.get("src", "contacts.txt");
+        List<String> contactsList = Files.readAllLines(contactsPath);
+        try {
+            contactsList = Files.readAllLines(contactsPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < contactsList.size(); i += 1) {
+            System.out.println((i + 1) + ": " + contactsList.get(i));
+        }
 
 
-//    }
+    }
 }
--- src/contactsManagerApp.java
+++ src/contactsManagerApp.java
@@ -1,8 +1,9 @@
 import java.awt.*;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class contactsManagerApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(contactManager.Menu());
     }
 
